81fafef73fe981cfe83b28507bd5c2ada3627e42
stephanmh <stephanmh@gmail.com>
2023-05-30 22:11:51 +0200

    Update harden.sh

--- root/harden.sh
+++ root/harden.sh
@@ -6,6 +6,6 @@ rm -fr /var/spool/cron && rm -fr /etc/crontabs && rm -fr /etc/periodic
 find /sbin /usr/sbin ! -type d -a ! -name nologin,chmod, -delete
 
 # Remove world-writable permissions.
-# This breaks apps that need to write to /tmp,
+# This no longer breaks apps that need to write to /tmp,
 # such as ssh-agent.
-find / -xdev -type d -perm +0002 -exec chmod o-w {} + && find / -xdev -type f -perm +0002 -exec chmod o-w {} +
+find / -xdev -type d -perm +0002 ! -name tmp -exec chmod o-w {} + && find / -xdev -type f -perm +0002 ! name tmp -exec chmod o-w {} +
