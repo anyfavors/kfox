47649a40a5657815533f85bb607a348d92c06979
stephanmh <stephanmh@gmail.com>
2023-05-30 22:14:20 +0200

    Update harden.sh

--- root/harden.sh
+++ root/harden.sh
@@ -8,4 +8,4 @@ find /sbin /usr/sbin ! -type d -a ! -name nologin,chmod, -delete
 # Remove world-writable permissions.
 # This no longer breaks apps that need to write to /tmp,
 # such as ssh-agent.
-find / -xdev -type d -perm +0002 ! -name tmp -exec chmod o-w {} + && find / -xdev -type f -perm +0002 ! name tmp -exec chmod o-w {} +
+find / -xdev -type d -perm +0002 ! -name tmp -exec chmod o-w {} + && find / -xdev -type f -perm +0002 ! -name tmp -exec chmod o-w {} +
